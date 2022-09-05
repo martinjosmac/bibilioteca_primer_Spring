4f16b936a1aa21973709701ee09c51ba864eebed
martin machado <martinmachado_86@hotmail.com>
2022-09-05 20:55:50 -0300

    otros modificadores

--- src/main/java/com/egg/biblioteca/controladores/EditorialControlador.java
+++ src/main/java/com/egg/biblioteca/controladores/EditorialControlador.java
@@ -1,12 +1,16 @@
 package com.egg.biblioteca.controladores;
 
+import com.egg.biblioteca.entidades.Editorial;
 import com.egg.biblioteca.excepciones.MiException;
 import com.egg.biblioteca.servicios.EditorialServicio;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -36,5 +40,22 @@ public class EditorialControlador {
         }
 
         return "index.html";
+    }
+
+    @GetMapping("/lista")
+    public String listar(ModelMap modelo) {
+
+        List<Editorial> editoriales = editorialServicio.listarEditoriales();
+
+        modelo.addAttribute("editoriales", editoriales);
+
+        return "editorial_list.html";
+    }
+
+    @GetMapping("/modificar/{id}")
+    public String modificar(@PathVariable String id, ModelMap modelo) {
+        modelo.put("editorial", editorialServicio.getOne(id));
+
+        return "editorial_modificar.html";
     }
 }
